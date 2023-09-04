e81581e5855ac6e64fa7b1f98b54c208f9602e76
yj.park <jolie0215@gmail.com>
2023-09-04 21:59:39 +0900

    init

--- src/main/java/com/anhae/drinkingLibrary/domain/User/User.java
+++ src/main/java/com/anhae/drinkingLibrary/domain/User/User.java
@@ -1,4 +1,19 @@
 package com.anhae.drinkingLibrary.domain.User;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="name", length = 32, nullable = false, unique = true)
+    private String email;
+    @Column(length = 32, nullable = false)
+    private String username;
 }
